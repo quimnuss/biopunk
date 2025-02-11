b5ef115c1ea16be7a56b0ecbe0a2e7a36fb57a23
Quim Nuss Székely <quimnuss@altaveu>
2025-02-11 20:53:41 +0100

    fix: nested machine

--- scripts/state_machine_nested.gd
+++ scripts/state_machine_nested.gd
@@ -5,19 +5,22 @@ class_name StateMachineNested
 
 @onready var state: State = get_initial_state()
 
-@export var pawn : Node3D
+var states : Dictionary
 
 func get_initial_state() -> State:
     return initial_state if initial_state != null else get_child(0)
 
 
 func _ready() -> void:
-    for state_node: State in find_children("*", "State", false):
-        state_node.papa = pawn
-        state_node.finished.connect(_transition_to_next_state)
+    assert(is_instance_valid(pawn))
 
-    await owner.ready
-    state.enter("")
+    for state_node: State in find_children("*", "State", false):
+        state_node.setup(pawn)
+        state_node.finished.connect(to_state)
+        states[state_node.id] = state_node
+
+    await pawn.ready
+    state.enter(null)
 
 
 func handle_input(_event:InputEvent) -> void:
@@ -32,20 +35,20 @@ func physics_update(_delta: float) -> void:
     state.physics_update(_delta)
 
 
-func enter(previous_state_path: String, data := {}) -> void:
-    state.enter(previous_state_path, data)
+func to_state(to_state_id: State.StateID, data: Dictionary = {}) -> void:
+    if to_state_id not in states:
+        printerr(pawn.name + ": Trying to transition to state " + State.state_names(to_state_id) + " but it does not exist.")
+        return
+
+    var previous_state := state
+    state.exit()
+    state = states[to_state_id]
+    state.enter(previous_state, data)
+
+
+func enter(_previous_state: State, _data := {}) -> void:
+    state.enter(_previous_state, _data)
 
 
 func exit() -> void:
     state.exit()
-
-
-func _transition_to_next_state(target_state_path: String, data: Dictionary = {}) -> void:
-    if not has_node(target_state_path):
-        printerr(pawn.name + ": Trying to transition to state " + target_state_path + " but it does not exist.")
-        return
-
-    var previous_state_path := state.name
-    state.exit()
-    state = get_node(target_state_path)
-    state.enter(previous_state_path, data)
