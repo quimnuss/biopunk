6feeb35bf39830cfd981548ff4147851c33dcedd
quimnuss <quimnuss@petit>
2025-02-13 20:23:11 +0100

    new: plants grow now

--- actors/plant.gd
+++ actors/plant.gd
@@ -1,13 +1,20 @@
 extends StaticBody3D
 class_name BallPlant
 @onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+@onready var age_timer : Timer = $AgeTimer
 
 var highlight_material : Material
+
+enum Ages { YOUNG, ADULT, OLD, ANCIENT, DEAD}
+
+var age : Ages = Ages.YOUNG
 
 signal picked_up(actor : Node3D)
 
 func _ready():
     highlight_material = mesh_instance_3d.material_overlay
+    self.scale = 0.5*Vector3.ONE
+
 
 func hurt():
     var tween : Tween = get_tree().create_tween()
@@ -27,3 +34,39 @@ func pickup():
 func release():
     # TODO set collision mask on release only
     pass
+
+func to_young():
+    var tween : Tween = create_tween()
+    var new_scale := 0.5*Vector3.ONE
+    tween.tween_property(self, "scale", new_scale, 1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+    tween.play()
+    age = Ages.YOUNG
+    
+func to_ancient():
+    var tween : Tween = create_tween()
+    var new_scale := 2*Vector3.ONE
+    tween.tween_property(self, "scale", new_scale, 1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+    tween.play()
+    age = Ages.ANCIENT
+
+func to_adult():
+    var tween : Tween = create_tween()
+    var new_scale := Vector3.ONE
+    tween.tween_property(self, "scale", new_scale, 1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+    tween.play()
+    age = Ages.ADULT
+
+
+func _on_age_timer_timeout():
+    match age:
+        Ages.YOUNG:
+            to_adult()
+        Ages.ADULT:
+            to_ancient()
+        Ages.ANCIENT:
+            to_young()
+        _:
+            pass
+            
+        
+        
